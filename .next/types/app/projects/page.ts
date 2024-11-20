1c4712c60e8487bb75dacb1f96ea085e69ee237a
dexterr6899 <yashtashildar6899@gmail.com>
2024-11-20 19:31:16 +0530

    major build failure fixes

--- .next/types/app/projects/page.ts
+++ .next/types/app/projects/page.ts
@@ -1,4 +1,4 @@
-// File: D:\YASH\Builds\yash-portfolio\YashTashildar\src\app\projects\page.tsx
+// File: D:\YASH\Builds\yash-portfolio\src\app\projects\page.tsx
 import * as entry from '../../../../src/app/projects/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
